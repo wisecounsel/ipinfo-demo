d79b516681ab5a465dab8d4d5ed53800df4cd7ef
Atharva Shukla <atharvashukla282@gmail.com>
2023-12-26 19:57:32 +0700

    refactor for vercel

--- api/index.js
+++ api/index.js
@@ -1,19 +1,25 @@
+// Import necessary modules
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const app = express();
-const port = 3000;
 
+// Initialize Express app
+const app = express();
+const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
+
+// Middleware for parsing JSON and serving static files from 'public'
 app.use(express.json());
 app.use(express.static("public"));
 
+// Route for the home page, serving index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Route to get IP information using ipinfo.io API
 app.get("/getIpInfo", async (req, res) => {
   const ip = req.query.ip;
-  const token = process.env.IPINFO_API;
+  const token = process.env.IPINFO_API; // Ensure to set IPINFO_API in your environment variables
 
   if (!ip) {
     return res.status(400).send({ message: "IP address is required!" });
@@ -30,6 +36,7 @@ app.get("/getIpInfo", async (req, res) => {
   }
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
